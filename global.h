c07998c34422b683e9064c6e66d8f8b692573d14
FishTofu <giaogiao521@163.com>
2025-04-05 16:02:54 +0800

    global.h fix

--- global.h
+++ global.h
@@ -2,11 +2,14 @@
 
 #include <opencv2/core.hpp>
 
-int width = 800, height = 600;
-cv::Vec3d eye(0., 0., 2.);
-cv::Vec3d center(0., 0., 0.);
-cv::Vec3d eyeup{0., -1., 0.};
-
-double fov = 45.0;
-double aspect = static_cast<double>(width) / height;
-double near = 0.1, far = 100.0;
+namespace global {
+extern double near;
+extern double far;
+extern int width;
+extern int height;
+extern cv::Vec3d eye;
+extern cv::Vec3d center;
+extern cv::Vec3d eyeup;
+extern double fov;
+extern double aspect;
+} // namespace global
